b7dcb166e81338f5d6626cf7c3329ebcca2e00ba
Emily Long <longe@byui.edu>
2019-06-06 23:12:33 -0600

    lots of changes

---
+++ topics/js-tutorial/js/calc.js
@@ -0,0 +1,15 @@
+function getHypotenuse() {
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+
+    var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+    document.getElementById('c').innerHTML = c;
+}
+
+var aa = parseFloat(document.getElementById('aa').innerHTML);
+var bb = parseFloat(document.getElementById('bb').innerHTML);
+
+var area = (aa * bb)/2;
+
+document.getElementById('area').innerHTML = area;
