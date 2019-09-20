7234ebe12c56b0451def5c4dd944c9797cd5f068
Emily Long <longe@byui.edu>
2019-09-19 21:00:17 -0600

    updates...

---
+++ topics/dom/js/add-image.js
@@ -0,0 +1,5 @@
+var myImg = document.createElement('IMG');
+myImg.setAttribute('src', 'images/flower.jpg');
+myImg.setAttribute('alt','Single Flower');
+myImg.setAttribute('class', 'testing');
+document.getElementsByTagName('main')[0].appendChild(myImg);
