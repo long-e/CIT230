501a79e631f2ac37170e35039ad91db9c1637472
Emily Long <longe@byui.edu>
2020-10-29 16:23:51 -0600

    updates

--- topics/dom/js/add-image.js
+++ topics/dom/js/add-image.js
@@ -3,3 +3,5 @@ myImg.setAttribute('src', 'images/flower.jpg');
 myImg.setAttribute('alt','Single Flower');
 myImg.setAttribute('class', 'testing');
 document.getElementsByTagName('main')[0].appendChild(myImg);
+
+footer2.innerHTML = 'testing...'
--- topics/js-tutorial/js/calc.js
+++ topics/js-tutorial/js/calc.js
@@ -3,13 +3,16 @@ function getHypotenuse() {
     var b = parseFloat(document.getElementById('b').value);
 
     var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    c = c.toFixed(2);
 
     document.getElementById('c').innerHTML = c;
+    
 }
 
 var aa = parseFloat(document.getElementById('aa').innerHTML);
 var bb = parseFloat(document.getElementById('bb').innerHTML);
 
 var area = (aa * bb)/2;
+area = area.toFixed(2);
 
 document.getElementById('area').innerHTML = area;
